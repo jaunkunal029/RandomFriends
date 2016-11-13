a48ca6f811bfa99cbbc8a30416db9ac0a16421bc
jaunkunal029 <jaunkunal029@gmail.com>
2016-11-13 21:18:30 +0530

    Add files via upload

---
+++ RandonFriends.py
@@ -0,0 +1,17 @@
+import os, random,subprocess,sys
+for i in range(sys.argv[1]):
+	string = "F:\TV series\F.R.I.E.N.D.S"
+	str1 = random.choice(os.listdir(string))
+	string = string + "\\"+str1
+	str2 = random.choice(os.listdir(string))
+	string = string + "\\"+ str2
+	fo = open("friendsplaylist.m3u8","a")
+	fo.write("\n"+string)
+	fo.close()
+	del string
+p = subprocess.Popen([r"C:/Program Files (x86)/VideoLAN/VLC/vlc.exe",r"C:\Users\KUNAL\friendsplaylist.m3u8"])
+
+p.wait()
+if(p.returncode == 0):
+	os.remove("friendsplaylist.m3u8")
+	
